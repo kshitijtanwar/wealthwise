a685a91c412e2c8682ee6371c308365c7b614e19
Yezaz <114501702+Yezaz@users.noreply.github.com>
2025-09-27 19:31:31 +0530

    Create Homepage.jsx

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,43 @@
+import HomepageNav from "../components/Homepage/HomepageNav";
+
+import About from "../components/Homepage/About"
+
+import Features from "../components/Homepage/Features";
+
+import Services from "../components/Homepage/Services";
+
+import Testimonials from "../components/Homepage/Testimonials";
+
+import Footer from "../components/Homepage/Footer";
+
+
+ 
+
+const Homepage = () => {
+
+    return <section>
+
+        <HomepageNav />
+
+        <div className="container-fluid">
+
+            <About />
+
+            <Features />
+
+            <Services />
+
+            <Testimonials />
+
+        </div>
+
+        <Footer />
+
+    </section>
+
+}
+
+
+ 
+
+export default Homepage;
